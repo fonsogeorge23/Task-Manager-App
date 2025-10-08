e3acc72b18fffe4756aa0edc440eeb4ba789f3d2
fonsogeorge23 <fonsogeorge@gmail.com>
2025-10-08 21:25:30 +0530

    Bug fix for JWT Token

---
+++ TaskManagementAPI/TaskManagementAPI/Services/Implementations/JwtServices.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TaskManagementAPI.Services.Interfaces;
+
+public class JwtServices : IJwtService
+{
+    private readonly IConfiguration _configuration;
+
+    public JwtServices(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    //public string GenerateToken(int userId, string username, string role)
+    //{
+    //    var claims = new[]
+    //    {
+    //        new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+    //        new Claim(ClaimTypes.Name, username),
+    //        new Claim(ClaimTypes.Role, role)  // used for [Authorize(Roles="Admin")]
+    //    };
+
+    //    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+    //    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+    //    var token = new JwtSecurityToken(
+    //        issuer: _configuration["Jwt:Issuer"],
+    //        audience: _configuration["Jwt:Audience"],
+    //        claims: claims,
+    //        expires: DateTime.UtcNow.AddHours(1),
+    //        signingCredentials: creds
+    //    );
+
+    //    return new JwtSecurityTokenHandler().WriteToken(token);
+    //}
+
+    public string GenerateToken(int userId, string username, string role)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes("9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08");
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+        new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+            new Claim(ClaimTypes.Name, username),
+            new Claim(ClaimTypes.Role, role)  // used for [Authorize(Roles="Admin")]
+            }),
+            Expires = DateTime.UtcNow.AddHours(1),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var jwtToken = tokenHandler.WriteToken(token);
+        return jwtToken;
+    }
+}
---
+++ TaskManagementAPI/TaskManagementAPI/Services/Interfaces/IJwtService.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskManagementAPI.Services.Interfaces
+{
+    public interface IJwtService
+    {
+        string GenerateToken(int userId, string username, string role);
+    }
+}
